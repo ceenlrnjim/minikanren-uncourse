f48cc3484d2efd101de234f8f41af3b7026f7c71
James Kirkwood <kirkwojl@hotmail.com>
2014-12-28 14:25:43 -0500

    added y combinator and myappend3 example

--- src/minikanren_uncourse/funcinterp.clj
+++ src/minikanren_uncourse/funcinterp.clj
@@ -43,6 +43,8 @@
 (defn extend-env [env k v]
   (cons [k v] env))
 
+(declare eval-exp)
+
 (defn eval-exp* [expr* env]
   (cond
     (empty? expr*) '()
@@ -67,10 +69,12 @@
 
     ; if conditional
     [(['if pred t f] :seq)]
-      (if (= :t (eval-exp pred env)) 
+      (if (eval-exp pred env) 
         (eval-exp t env)
         (eval-exp f env))
 
+    ; null?
+    [(['null? e] :seq)] (if (empty? (eval-exp e env)) :t :f)
 
     ; empty list
     [([] :seq)] '()
@@ -83,7 +87,8 @@
          (eval-exp* exprs env)
 
     ; cons
-    [(['cons h (t :guard list?)] :seq)] (list (eval-exp h env) (eval-exp t env))
+    [(['cons h (t :guard list?)] :seq)] 
+       (cons (eval-exp h env) (eval-exp t env))
          
     ; car
     [(['car (l :guard list?)] :seq)] (first (eval-exp l env))
@@ -92,8 +97,6 @@
     [(['cdr (l :guard list?)] :seq)] (second (eval-exp l env))
 
     ; TODO: bool? zero?
-    ; TODO: cons car cdr
-    ; TODO: quote list
     ; TODO: tagged numbers and arithmatic
     
           
@@ -125,6 +128,34 @@
     [_] (throw (IllegalArgumentException. (str "Invalid expression: " expr)))))
 
 
+; Y combinator for eagerly evaluated, call by value, language
+; "inverse eta"
+;  Idea of eta: (lambda (x) (add1 x)) is the same as add1 (eta contraction)
+;  so inverse eta: add1 => (lambda (x) (add1 x))  (eta expansion)
+;
+;  Mathematical definition of Y is:
+;  Y x = x (Y x) fixed point combinator
+(defn Y [f]
+  ((fn [x] (f (x x))) 
+   (fn [x] (fn [y] ((f (x x)) y)))))
+
+
+; replacing with a new definition to make implementation in our interpreter simpler
+(defn myappend2 [l]
+  ; need to curry since our interpreter only takes one argument
+  (fn [s]
+    (if (empty? l) 
+      s
+      (cons (first l) ((myappend2 (rest l)) s)))))
+
+(((Y (fn [myappend3]
+      (fn [l]
+        (fn [s]
+          (if (empty? l) 
+            s
+            (cons (first l) ((myappend3 (rest l)) s)))))))
+ '(a b c)) '(d e))
+
 ; examples of variable lookup
 (comment
   
@@ -153,8 +184,8 @@
   (eval-exp '(let [foo (λ [x] x)] (foo 100)) [])
 
   ; if and booleans
-  (eval-exp '(if y 1 0) [['y :t]])
-  (eval-exp '(if y 1 0) [['y :f]])
+  (eval-exp '(if y 1 0) [['y true]])
+  (eval-exp '(if y 1 0) [['y false]])
 
   (eval-exp '(quote (1 2 3 4)) [])
   (eval-exp '(quote (λ (x) (λ (y) (cons x (cons y (quote ())))))) [])
